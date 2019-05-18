0d5ebc0f97c49b3af394205e5a7761a55b0a6de1
annoyan <yysan515@gmail.com>
2019-05-18 17:11:23 +0800

    rev1.3.2
    Solve problem

--- predict/netlaprls/predictmodel.py
+++ predict/netlaprls/predictmodel.py
@@ -33,14 +33,16 @@ scores = model.predict_scores(zip(x, y), 5)
 # ii 表示将预测到的scores列表 进行降序
 ii = np.argsort(scores)[::-1]
 # 预测返回的样本个数
-predict_num = None
+predict_num = 1000
 
 output_dir = os.path.join(os.path.abspath('../'), 'output')
 print 'output_dir is ' + output_dir
 # TODO 预测标靶对
 # 预测标靶对
 predict_pairs = [(drug_names[x[i]], target_names[y[i]], scores[i]) for i in ii[:predict_num]]
-new_dti_file = os.path.join(output_dir, "_".join(['netlaprls', dataset, "new_dti.txt"]))
+for i in predict_pairs:
+    print i
+# new_dti_file = os.path.join(output_dir, "_".join(['netlaprls', dataset, "new_dti.txt"]))
 # novel_prediction_analysis(predict_pairs, new_dti_file, os.path.join(data_dir, 'biodb'))
 # '''
 #     在现有的数据集中预测新的药物-标靶作用对
